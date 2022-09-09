c885355cce80feb50d1a3399d3efe5fec13c9d18
Leonid <leonidkoscheev984@gmail.com>
2022-09-09 13:49:16 +0200

    add store

--- src/index.js
+++ src/index.js
@@ -5,22 +5,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import state from './state/state';
-import { addPost } from './state/state';
-import { subcribe } from './state/state';
+import store from './state/state';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 let mainRender = (state) => {
   root.render(
     <React.StrictMode>
       <BrowserRouter>
-        <App dialogPage={state.dialogPage} profilePage={state.profilePage} addPost={addPost} />
+        <App dialogPage={state.dialogPage} profilePage={state.profilePage} addPost={store.addPost.bind(store)} />
       </BrowserRouter>
     </React.StrictMode>
   );
 };
-mainRender(state);
-subcribe(mainRender); 
+mainRender(store.getState());
+store.subcribe(mainRender); 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/state/state.js
+++ src/state/state.js
@@ -1,39 +1,48 @@
 import Me from '../image/me.jpg';
-let state = { // база данных
-    profilePage: {
-        postData: [
-            { text: 'Hello', id: 1 },
-            { text: 'Bye', id: 2 },
-            { text: 'Didnt ask', id: 3 },
-            { text: 'Good', id: 4 }
-        ],
+let store = {
+    _state: { // база данных
+        profilePage: {
+            postData: [
+                { text: 'Hello', id: 1 },
+                { text: 'Bye', id: 2 },
+                { text: 'Didnt ask', id: 3 },
+                { text: 'Good', id: 4 }
+            ],
+        },
+        dialogPage: {
+            messageContent: [
+                { img: { Me }, name: 'Leonid', id: 1, text: 'Hello' },
+                { img: { Me }, name: 'Andrey', id: 2, text: 'No' },
+                { img: { Me }, name: 'Shasha', id: 3, text: 'Yes' },
+                { img: { Me }, name: 'Aleksey', id: 4, text: 'K' },
+            ],
+            dialogNames: [
+                { name: 'elon mysk', id: 1 },
+                { name: 'leonid', id: 2 },
+                { name: 'donald trump', id: 3 },
+                { name: 'tim kuk', id: 4 }
+            ]
+        }
+    }, //конец баззы данных
+    getState() { //функция которая возвращает state 
+        return(
+            this._state
+        )
     },
-    dialogPage: {
-        messageContent: [
-            { img: { Me }, name: 'Leonid', id: 1, text: 'Hello' },
-            { img: { Me }, name: 'Andrey', id: 2, text: 'No' },
-            { img: { Me }, name: 'Shasha', id: 3, text: 'Yes' },
-            { img: { Me }, name: 'Aleksey', id: 4, text: 'K' },
-        ],
-        dialogNames: [
-            { name: 'elon mysk', id: 1 },
-            { name: 'leonid', id: 2 },
-            { name: 'donald trump', id: 3 },
-            { name: 'tim kuk', id: 4 }
-        ]
+    mainRender() { // функция в которую попадет функция для обрисовки / рендера  всей социальной сети.
+        
+    },
+    addPost (postText) { // создем стрелочную функцию addPost которая принимает инфо. postText и экспортируем её. запускаем эту функцию в момент нажатия на кнопку опубликовать
+        let newPost = { // объект в который попадает текст поста и его id.
+            text: postText,
+            id: 5,
+        };
+        this.getState().profilePage.postData.splice(0, 0, newPost); //добавляем новый пост в раздел в баз. данных в postData
+        this.mainRender(this.getState()); //запускаем функцию для прорисовки всей соц. сети и передаем обновленную баз. данных
+    },
+    subcribe (opserver) { // создаем функцию subscribe которая принимает информацию oppServer. запускаем эту функцию при обновлении страницы
+        this.mainRender = opserver; //присваем mainRender opServer. 
     }
-} //конец баззы данных
-let mainRender; // переменная в которую попадет функция для обрисовки / рендера  всей социальной сети.
-export let addPost = (postText) => { // создем стрелочную функцию addPost которая принимает инфо. postText и экспортируем её. запускаем эту функцию в момент нажатия на кнопку опубликовать
-    let newPost = { // объект в который попадает текст поста и его id.
-        text: postText,
-        id: 5,
-    };
-    state.profilePage.postData.push(newPost); //добавляем новый пост в раздел в баз. данных в postData
-    mainRender(state); //запускаем функцию для прорисовки всей соц. сети и передаем обновленную баз. данных
-};
- 
-export let subcribe = (opserver) => { // создаем функцию subscribe которая принимает информацию oppServer. запускаем эту функцию при обновлении страницы
-    mainRender = opserver; //присваем mainRender opServer. 
-};
-export default state; -
+}
+
+export default store; 
